b072b552efc421ba1cddb437effc4c3c364cf745
iva <ivalulyanov1@gmail.com>
2024-07-16 18:48:14 +0300

    new module

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Request, Form
+from fastapi.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name='static')
+
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {'request': request})
+
+
+@app.get("/class_selection")
+async def class_selection(request: Request):
+    return templates.TemplateResponse("class_selection.html", {'request': request})
+
+
+@app.post("/class_selection/{class_id}")
+async def class_selection_form(request: Request, class_id: Annotated[str, Form()]):
+    return templates.TemplateResponse('task_selection.html', {'request': request, 'class_id': class_id})
---
+++ src/models/models.py
@@ -0,0 +1,5 @@
+"""
+Models
+"""
+
+from pydantic import BaseModel
