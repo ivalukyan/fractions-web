c05f1c6dc5661fa2bb66eb1a8c2c3a4d968265bd
ivalukyan <ivalukyanov@gmail.com>
2024-08-08 18:07:16 +0300

    new check in auth

--- backend/app/operations/auth/auth.py
+++ backend/app/operations/auth/auth.py
@@ -8,6 +8,7 @@ from app.database.db import Student, Teacher
 from app.operations.auth import templates
 
 from env import Admin
+from app.utils.utils import email_check, password_check
 
 router = APIRouter(tags=["auth"])
 
@@ -22,41 +23,52 @@ async def auth(request: Request):
 @router.post("/auth_student")
 async def auth_student(request: Request, email: Annotated[str, Form()], password: Annotated[str, Form()]):
 
-    db_session = Session()
-    student = db_session.query(Student).filter(Student.email == email).first()
+    if email_check(email) and password_check(password):
 
-    unauth = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Bad request')
-    msg = 'Ivalid email or password'
+        db_session = Session()
+        student = db_session.query(Student).filter(Student.email == email).first()
 
-    if student is not None:
-        if password in student.password:
-            redirect_url = request.url_for('home_user', email_student=email)
-            return RedirectResponse(redirect_url)
+        unauth = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Bad request')
+        msg = 'Неправильный логин или пароль'
+
+        if student is not None:
+            if password in student.password:
+                redirect_url = request.url_for('home_user', email_student=email)
+                return RedirectResponse(redirect_url)
+            else:
+                return templates.TemplateResponse("auth/auth.html", {'request': request, 'msg': msg})
         else:
             return templates.TemplateResponse("auth/auth.html", {'request': request, 'msg': msg})
     else:
+        msg = 'Неправильный логин или пароль'
         return templates.TemplateResponse("auth/auth.html", {'request': request, 'msg': msg})
 
 
 @router.post("/auth_teacher")
 async def auth_teacher(request: Request, email: Annotated[str, Form()], password: Annotated[str, Form()]):
 
-    db_session = Session()
-    teacher = db_session.query(Teacher).filter(Teacher.email == email).first()
+    if email_check(email) and password_check(email):
 
-    unauth = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Bad request')
-    msg = 'Неправильный логин или пароль!'
+        db_session = Session()
+        teacher = db_session.query(Teacher).filter(Teacher.email == email).first()
 
-    if email in admin.username and password in admin.password:
-        redirect_url = request.url_for('home_teacher', email_teacher=email)
-        return RedirectResponse(redirect_url)
-    else:
-        if teacher is not None:
+        unauth = HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Bad request')
+        msg = 'Неправильный логин или пароль!'
 
-            if password in teacher.password:
-                redirect_url = request.url_for('home_teacher', email_teacher=email)
-                return RedirectResponse(redirect_url)
-            else:
-                return templates.TemplateResponse('auth/auth.html', {'request': request, 'msg': msg})
+        if email in admin.username and password in admin.password:
+            redirect_url = request.url_for('home_teacher', email_teacher=email)
+            return RedirectResponse(redirect_url)
         else:
-            return templates.TemplateResponse("auth/auth.html", {'request': request, 'msg': msg, 'mode':'teacher'})
+            if teacher is not None:
+
+                if password in teacher.password:
+                    redirect_url = request.url_for('home_teacher', email_teacher=email)
+                    return RedirectResponse(redirect_url)
+                else:
+                    return templates.TemplateResponse('auth/auth.html', {'request': request, 'msg': msg})
+            else:
+                return templates.TemplateResponse("auth/auth.html", {'request': request, 'msg': msg, 'mode': 'teacher'})
+
+    else:
+        msg = 'Неправильный логин или пароль'
+        return templates.TemplateResponse("auth/auth.html", {'request': request, 'msg': msg, 'mode': 'teacher'})
--- backend/app/utils/utils.py
+++ backend/app/utils/utils.py
@@ -2,11 +2,21 @@ import re
 
 
 def answer_check(answer: str) -> bool:
-    valid = r'^([А-ЯЁ]*[а-яё]*|\s*|[0-9]*)$'
+    valid = r"^([А-ЯЁ]*[а-яё]*|\s*|[0-9]*)$"
     if re.match(valid, answer):
         return True
     return False
 
 
-if __name__ == '__main__':
-    pass
+def email_check(email: str) -> bool:
+    valid = r"^[a-zA-Z0-9_.+-]+@[a-z]+\.[a-z]+$"
+    if re.match(valid, email):
+        return True
+    return False
+
+
+def password_check(password: str) -> bool:
+    valid = r"^[-#&!$@a-zA-Z0-9]+$"
+    if re.match(valid, password):
+        return True
+    return False
